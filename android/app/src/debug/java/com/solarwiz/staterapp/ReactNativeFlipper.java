c59ab34287f5a17fd134843a527518b6c537df9c
jkyoo-pc\jkyoo <jk.yoo@solarwiz.co.kr>
2023-01-06 19:54:28 +0900

    android pakcage name change

--- android/app/src/debug/java/com/solarwiz/staterapp/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/solarwiz/staterapp/ReactNativeFlipper.java
@@ -4,7 +4,9 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.staterapp;
+package com.solarwiz.staterapp;
+
+
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
